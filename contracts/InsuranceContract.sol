548ef7d8711f707cc73206896bcbb12235a695c1
AKA-Muhannad <muhannadalseraye@gmail.com>
2023-09-10 01:05:34 +0300

    upload first contract code

--- contracts/InsuranceContract.sol
+++ contracts/InsuranceContract.sol
@@ -1,26 +1,63 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract InsuranceContract is ERC20, Ownable {
-    address userAddress;
+    address public insuranceAddress = 0xd7b5B1e52369F1402Ba350d7f5Dd3D247717c157; // Osama's insurance company address
+    uint public registeredInsurance; // number of insurance
+    uint public registeredCustomer; // number of customers
+
     constructor() ERC20("Muhannad Insurance", "MIT") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
-    mapping (address => User) public users;
 
-    struct User{
+    mapping(address => User) public users;
+
+    struct User {
         string userName;
         address userAddress;
         uint userAge;
         uint userBalance;
+        uint startInsurance;
+        uint endInsurance;
     }
-    modifier userCheck{
-        require(userAddress == msg.sender, "You are not the user");_;
+    modifier checkUser() {
+        require(
+            users[msg.sender].userAddress == msg.sender,
+            "You don't have membership"
+        );
+        _;
+    }
+    modifier checkBalance() {
+        require(
+            //                              1 as default amount
+            users[msg.sender].userBalance >= 1,
+            "You don't have balance enough"
+        );
+        _;
     }
 
+    function registeCustomer(string memory newName, uint newAge) public {
+        uint newBalance = balanceOf(msg.sender);
+        users[msg.sender] = User(
+            newName,
+            msg.sender,
+            newAge,
+            newBalance,
+            0,
+            0
+        );
+        registeredCustomer++;
+    }
+    function registeInsurance() public {
+        //           from me to osama with 1 Wei   
+        transferFrom(msg.sender, insuranceAddress, 1);
+        users[msg.sender].startInsurance = block.timestamp; // time of start the registration which's now
+        users[msg.sender].endInsurance = block.timestamp + 31556926; // time of end the registration which's after 1 year
+        registeredInsurance++;
+    }
 }
